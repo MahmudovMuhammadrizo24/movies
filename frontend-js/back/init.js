9564094c59478f5c9f76bb20164d476814883e15
MahmudovMuhammadrizo24 <mahmudovmuhammadrizo877@gmail.com>
2023-06-02 05:26:34 +0500

    add js code

--- frontend-js/back/init.js
+++ frontend-js/back/init.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 app.use(express.json());
@@ -11,4 +11,225 @@ app.post('/api/data', (req, res) => {
 
 app.listen(3000, () => {
     console.log('Server ishga tushdi');
+});*/
+
+let movieCount = 0;
+const table = document.querySelector < HTMLTableElement > ('.col .table');
+const movieCountText = document.querySelector < HTMLSpanElement > ('.col .movie-count');
+const movieContainer = document.querySelector < HTMLDivElement > ('.container');
+const movieLoader = document.querySelector('.loader');
+movieCountText!.textContent = movieCount.toString();
+const moviesSearch = document.querySelector < HTMLInputElement > ('.form-control');
+const moviesGenresAll = document.querySelectorAll < HTMLLIElement > ('.list-group-item');
+const sortClick = document.querySelectorAll < HTMLIFrameElement > ('.sort-click');
+const sortIcons = document.querySelectorAll < HTMLIFrameElement > ('.sort-icon');
+const moviePagination = document.querySelector < HTMLUListElement > ('.pagination');
+const moviesPerPage = 4;
+let currentPage = 1;
+let selectedGenres: string[] = [];
+let moviesData: Array<{
+    title: string;
+    genre: {
+        name: string;
+    };
+    numberInStock: number;
+    dailyRentalRate: number;
+}> = [];
+
+sortIcons.forEach(icon => {
+    icon.classList.add('d-none');
 });
+
+sortClick.forEach((sort, index) => {
+    sort.addEventListener('click', () => {
+        sortIcons.forEach((icon, iconIndex) => {
+            if (index === iconIndex) {
+                icon.classList.toggle('fa-sort-up');
+                icon.classList.toggle('fa-sort-down');
+            } else {
+                icon.classList.add('d-none');
+                icon.classList.remove('fa-sort-up');
+                icon.classList.remove('fa-sort-down');
+            }
+        });
+    });
+});
+
+movieContainer?.classList.add('container-loading');
+movieLoader?.classList.remove('container-loading');
+
+fetch('https://pdp-movies-78.onrender.com/api/movies')
+    .then(response => response.json())
+    .then(movies => {
+        movieCount = movies.length;
+        movieCountText!.textContent = movieCount.toString();
+        moviesData = movies;
+        renderMovies();
+        updatePagination();
+        console.log(movies);
+        movieContainer?.classList.remove('container-loading');
+        movieLoader?.classList.add('container-loading');
+    })
+    .catch(error => {
+        console.log('Xatolik yuz berdi:', error);
+    });
+
+function renderMovies() {
+    const startIndex = (currentPage - 1) * moviesPerPage;
+    const endIndex = startIndex + moviesPerPage;
+    const moviesToRender = getFilteredMovies().slice(startIndex, endIndex);
+
+    while (table!.rows.length > 1) {
+        table!.deleteRow(1);
+    }
+
+    moviesToRender.forEach(movie => {
+        createTable(movie);
+    });
+}
+
+function createTable(movie) {
+    const row = document.createElement('tr');
+
+    const movieName = document.createElement('td');
+    const tdAnchor = document.createElement('a');
+    tdAnchor.href = '';
+    tdAnchor.className = 'movie-name'
+    tdAnchor.textContent = movie.title;
+    movieName.appendChild(tdAnchor);
+    row.appendChild(movieName);
+
+    const movieGenre = document.createElement('td');
+    movieGenre.textContent = movie.genre.name;
+    movieGenre.className = 'movie-genre'
+    row.appendChild(movieGenre);
+
+    const movieNumberInStock = document.createElement('td');
+    movieNumberInStock.textContent = movie.numberInStock;
+    row.appendChild(movieNumberInStock);
+
+    const movieDailyRentalRate = document.createElement('td');
+    movieDailyRentalRate.textContent = movie.dailyRentalRate;
+    row.appendChild(movieDailyRentalRate);
+
+    const movieFavoriteHeart = document.createElement('td');
+    const movieHeartIcon = document.createElement('i');
+    movieHeartIcon.className = 'icon-heart fa-regular fa-heart';
+    movieFavoriteHeart.appendChild(movieHeartIcon);
+    row.appendChild(movieFavoriteHeart);
+
+    let isLiked = false;
+
+    movieHeartIcon.addEventListener('click', () => {
+        if (isLiked) {
+            movieHeartIcon.classList.replace('fa-solid', 'fa-regular');
+            isLiked = false;
+        } else {
+            movieHeartIcon.classList.replace('fa-regular', 'fa-solid');
+            isLiked = true;
+        }
+    });
+
+    table!.appendChild(row);
+}
+
+moviesSearch?.addEventListener('input', () => {
+    handleSearch();
+});
+
+
+
+
+moviesGenresAll.forEach(genre => {
+    genre.addEventListener('click', () => {
+        moviesGenresAll.forEach(g => {
+            g.classList.remove('active');
+        });
+        genre.classList.add('active');
+        handleGenreSelection();
+    });
+});
+
+
+
+function handleGenreSelection() {
+    selectedGenres = [];
+    moviesGenresAll.forEach(genre => {
+        if (genre.classList.contains('active')) {
+            selectedGenres.push(genre.textContent || '');
+        }
+    });
+
+    if (selectedGenres.includes('All Genres')) {
+        selectedGenres = [];
+    }
+
+    renderMovies();
+    updatePagination();
+}
+
+
+function getFilteredMovies() {
+    const searchQuery = moviesSearch!.value.toLowerCase();
+
+    return moviesData.filter(movie => {
+        const title = movie.title.toLowerCase();
+        const genre = movie.genre.name.toLowerCase();
+
+        const isTitleMatch = title.includes(searchQuery);
+        const isGenreMatch =
+            selectedGenres.length === 0 || selectedGenres.some(selectedGenre => genre.includes(selectedGenre.toLowerCase()));
+
+        return isTitleMatch && isGenreMatch;
+    });
+}
+
+
+
+function handleSearch() {
+    renderMovies();
+    if (moviesSearch!.value === '') {
+        currentPage = 1;
+        updatePagination();
+    } else {
+        currentPage = 1;
+        renderMovies();
+        updatePagination();
+    }
+}
+
+function updatePagination() {
+    const filteredMoviesCount = getFilteredMovies().length;
+    const totalPages = Math.ceil(filteredMoviesCount / moviesPerPage);
+
+    currentPage = Math.min(currentPage, totalPages);
+
+    while (moviePagination!.firstChild) {
+        moviePagination!.removeChild(moviePagination!.firstChild);
+    }
+
+    const allGenresActive = selectedGenres.length === 0;
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageItem = document.createElement('li');
+        pageItem.classList.add('page-item');
+        const pageLink = document.createElement('a');
+        pageLink.classList.add('page-link');
+        pageLink.textContent = i.toString();
+        pageLink.href = '#';
+        // if (i === currentPage || (i === 1 && allGenresActive)) {
+        //   pageItem.classList.add('active');
+        // }
+        pageItem.appendChild(pageLink);
+        moviePagination!.appendChild(pageItem);
+    }
+
+    const pageLinks = moviePagination!.querySelectorAll('.page-link');
+    pageLinks.forEach(pageLink => {
+        pageLink.addEventListener('click', () => {
+            currentPage = parseInt(pageLink.textContent!);
+            renderMovies();
+            handleSearch();
+        });
+    });
+} 
--- frontend-js/list-group.js
+++ frontend-js/list-group.js
@@ -1,6 +1,8 @@
-const list_group_item = document.querySelectorAll(".list-group-item");
+/*const list_group_item = document.querySelectorAll(".list-group-item");
 const bigBox = document.querySelector(".bigBox")
-
+const form_control = document.querySelector(".form-control");
+const moviNum = document.querySelector(".moviNum");
+const containerDiv = document.querySelector(".film");
 
 
 for (let listGroup of list_group_item) {
@@ -11,40 +13,30 @@ for (let listGroup of list_group_item) {
                 otherListGroup.classList.remove("active");
             }
         }
-
         listGroup.classList.add("active");
     });
 }
-/*
-fetch("https://pdp-movies-78.onrender.com/api/movies")
-    .then((response) => response.json())
-    .then((data) => {
-        // Ma'lumotlar bilan ishlash
-        console.log(data); // Ma'lumotlarni konsolga chiqarish
-        // ...
-    })
-    .catch((error) => {
-        console.error("Xatolik yuz berdi:", error);
-    });
-*/
 
-fetch("https://pdp-movies-78.onrender.com/api/movies")
-    .then((response) => response.json())
-    .then((data) => {
+list_group_item.forEach(item => {
+    item.addEventListener('click', () => {
+        const selectedGenre = item.textContent;
+        fetch("https://pdp-movies-78.onrender.com/api/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                const genreMovies = [];
+                for (let i = 0; i < data.length; i++) {
+                    if (selectedGenre === 'All Genres' || data[i].genre.name === selectedGenre) {
+                        genreMovies.push(data[i]);
+                    }
+                }
+                displayGenreMovies(genreMovies);
+                moviNum.textContent = genreMovies.length;
+            })
+
+
         const colDiv = document.createElement("div");
         colDiv.className = "col";
-        /*
-                const pTag = document.createElement("p");
-                pTag.textContent = "Showing 12 movies in the database.";
-                colDiv.appendChild(pTag);
-        
-                const input = document.createElement("input");
-                input.type = "text";
-                input.name = "query";
-                input.className = "form-control my-3";
-                input.placeholder = "Search...";
-                colDiv.appendChild(input);
-        */
+
         const table = document.createElement("table");
         table.className = "table";
 
@@ -74,42 +66,63 @@ fetch("https://pdp-movies-78.onrender.com/api/movies")
         th4.appendChild(iTag);
         tr.appendChild(th4);
 
+
         const th5 = document.createElement("th");
         th5.className = "clickable";
+        th5.textContent = "like";
         tr.appendChild(th5);
 
         thead.appendChild(tr);
         table.appendChild(thead);
 
-        const tbody = document.createElement("tbody");
-        data.forEach((movie) => {
-            const movieRow = document.createElement("tr");
 
-            const titleCell = document.createElement("td");
-            const titleLink = document.createElement("a");
-            titleLink.href = `/movies/${movie.id}`;
-            titleLink.textContent = movie.title;
-            titleCell.appendChild(titleLink);
-            movieRow.appendChild(titleCell);
+        function displayGenreMovies(data) {
+            const tbody = document.createElement("tbody");
+            data.forEach((movie) => {
+                const movieRow = document.createElement("tr");
 
-            const genreCell = document.createElement("td");
-            genreCell.textContent = movie.genre.name;
-            movieRow.appendChild(genreCell);
+                const titleCell = document.createElement("td");
+                const titleLink = document.createElement("a");
+                titleLink.href = `/movies/${movie.id}`;
+                titleLink.textContent = movie.title;
+                titleCell.appendChild(titleLink);
+                movieRow.appendChild(titleCell);
 
-            const stockCell = document.createElement("td");
-            stockCell.textContent = movie.numberInStock;
-            movieRow.appendChild(stockCell);
+                const genreCell = document.createElement("td");
+                genreCell.textContent = movie.genre.name;
+                movieRow.appendChild(genreCell);
 
-            const rateCell = document.createElement("td");
-            rateCell.textContent = movie.dailyRentalRate;
-            movieRow.appendChild(rateCell);
+                const stockCell = document.createElement("td");
+                stockCell.textContent = movie.numberInStock;
+                movieRow.appendChild(stockCell);
 
-            const emptyCell = document.createElement("td");
-            movieRow.appendChild(emptyCell);
+                const rateCell = document.createElement("td");
+                rateCell.textContent = movie.dailyRentalRate;
+                movieRow.appendChild(rateCell);
 
-            tbody.appendChild(movieRow);
-        });
 
+                const emptyCell = document.createElement("td");
+
+                const iconElement = document.createElement("i");
+                iconElement.className = "fa-regular fa-heart";
+                iconElement.style.color = "#04070d";
+
+                // Add event listener to toggle the icon
+                iconElement.addEventListener("click", function () {
+                    if (iconElement.className.includes("fa-regular")) {
+                        iconElement.className = "fa-solid fa-heart";
+                    } else {
+                        iconElement.className = "fa-regular fa-heart";
+                    }
+                });
+                emptyCell.appendChild(iconElement);
+                movieRow.appendChild(emptyCell);
+
+
+
+                tbody.appendChild(movieRow);
+            });
+        }
         table.appendChild(tbody);
         colDiv.appendChild(table);
 
@@ -117,28 +130,42 @@ fetch("https://pdp-movies-78.onrender.com/api/movies")
         const ul = document.createElement("ul");
         ul.className = "pagination";
 
-        for (let i = 1; i <= 3; i++) {
+
+        for (let i = 1; i <= 4; i++) {
             const li = document.createElement("li");
-            li.className = i === 1 ? "page-item active" : "page-item";
+            li.className = "page-item";
+
             const a = document.createElement("a");
             a.className = "page-link";
             a.textContent = i;
 
             li.appendChild(a);
             ul.appendChild(li);
+
+            li.addEventListener("click", function () {
+                ul.querySelectorAll(".page-item").forEach(function (item) {
+                    item.classList.remove("active");
+                });
+
+                li.classList.add("active");
+
+                // Additional actions when the li is toggled
+            });
         }
+
 
         nav.appendChild(ul);
         colDiv.appendChild(nav);
 
         // Append the created elements to the desired container on your page
-        const containerDiv = document.querySelector(".film");
+
         containerDiv.appendChild(colDiv);
     })
-    .catch((error) => {
-        console.error("Xatolik yuz berdi:", error);
-    });
+        .catch((error) => {
+            console.error("Xatolik yuz berdi:", error);
+        });
 
+});
 
 
 input.addEventListener("input", () => {
@@ -171,10 +198,175 @@ movieRow.appendChild(genreCell);
 
 
 
+form_control.value = movie.title;
+*/
 
 
 
 
 
+const list_group_item = document.querySelectorAll(".list-group-item");
+const bigBox = document.querySelector(".bigBox");
+const form_control = document.querySelector(".form-control");
+const moviNum = document.querySelector(".moviNum");
+const containerDiv = document.querySelector(".film");
+const tbody = document.createElement("tbody");
 
+for (let listGroup of list_group_item) {
+    listGroup.addEventListener("click", () => {
+        for (let otherListGroup of list_group_item) {
+            if (otherListGroup !== listGroup) {
+                otherListGroup.classList.remove("active");
+            }
+        }
+        listGroup.classList.add("active");
 
+        const selectedGenre = listGroup.textContent;
+        fetch("https://pdp-movies-78.onrender.com/api/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                const genreMovies = [];
+                for (let i = 0; i < data.length; i++) {
+                    if (selectedGenre === 'All Genres' || data[i].genre.name === selectedGenre) {
+                        genreMovies.push(data[i]);
+                    }
+                }
+                displayGenreMovies(genreMovies);
+                moviNum.textContent = genreMovies.length;
+            })
+            .catch((error) => {
+                console.error("Xatolik yuz berdi:", error);
+            });
+    });
+}
+
+function displayGenreMovies(data) {
+    tbody.innerHTML = '';
+
+    data.forEach((movie) => {
+        const movieRow = document.createElement("tr");
+
+        const titleCell = document.createElement("td");
+        const titleLink = document.createElement("a");
+        titleLink.href = `/movies/${movie.id}`;
+        titleLink.textContent = movie.title;
+        titleCell.appendChild(titleLink);
+        movieRow.appendChild(titleCell);
+
+        const genreCell = document.createElement("td");
+        genreCell.textContent = movie.genre.name;
+        movieRow.appendChild(genreCell);
+
+        const stockCell = document.createElement("td");
+        stockCell.textContent = movie.numberInStock;
+        movieRow.appendChild(stockCell);
+
+        const rateCell = document.createElement("td");
+        rateCell.textContent = movie.dailyRentalRate;
+        movieRow.appendChild(rateCell);
+
+        const emptyCell = document.createElement("td");
+        const iconElement = document.createElement("i");
+        iconElement.className = "fa-regular fa-heart";
+        iconElement.style.color = "#04070d";
+
+        // Add event listener to toggle the icon
+        iconElement.addEventListener("click", function () {
+            if (iconElement.className.includes("fa-regular")) {
+                iconElement.className = "fa-solid fa-heart";
+            } else {
+                iconElement.className = "fa-regular fa-heart";
+            }
+        });
+        emptyCell.appendChild(iconElement);
+        movieRow.appendChild(emptyCell);
+
+        tbody.appendChild(movieRow);
+    });
+}
+
+const table = document.createElement("table");
+table.className = "table";
+
+const thead = document.createElement("thead");
+const tr = document.createElement("tr");
+
+const th1 = document.createElement("th");
+th1.className = "clickable";
+th1.textContent = "Title";
+tr.appendChild(th1);
+
+const th2 = document.createElement("th");
+th2.className = "clickable";
+th2.textContent = "Genre";
+tr.appendChild(th2);
+
+const th3 = document.createElement("th");
+th3.className = "clickable";
+th3.textContent = "Stock";
+tr.appendChild(th3);
+
+const th4 = document.createElement("th");
+th4.className = "clickable";
+th4.textContent = "Rate";
+const iTag = document.createElement("i");
+iTag.className = "fa fa-sort-desc";
+th4.appendChild(iTag);
+tr.appendChild(th4);
+
+const th5 = document.createElement("th");
+th5.className = "clickable";
+th5.textContent = "like";
+tr.appendChild(th5);
+
+thead.appendChild(tr);
+table.appendChild(thead);
+table.appendChild(tbody);
+
+const colDiv = document.createElement("div");
+colDiv.className = "col";
+colDiv.appendChild(table);
+
+const nav = document.createElement("nav");
+const ul = document.createElement("ul");
+ul.className = "pagination";
+
+for (let i = 1; i <= 4; i++) {
+    const li = document.createElement("li");
+    li.className = "page-item";
+
+    const a = document.createElement("a");
+    a.className = "page-link";
+    a.textContent = i;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+
+    li.addEventListener("click", function () {
+        ul.querySelectorAll(".page-item").forEach(function (item) {
+            item.classList.remove("active");
+        });
+
+        li.classList.add("active");
+
+        // Additional actions when the li is toggled
+    });
+}
+
+nav.appendChild(ul);
+colDiv.appendChild(nav);
+
+containerDiv.appendChild(colDiv);
+
+form_control.addEventListener("input", () => {
+    const searchQuery = form_control.value.toLowerCase();
+    tbody.innerHTML = "";
+
+    const genreMovies = Array.from(tbody.children);
+    genreMovies.forEach((movie) => {
+        const movieTitle = movie.firstChild.firstChild.textContent.toLowerCase();
+        if (movieTitle.includes(searchQuery)) {
+            tbody.appendChild(movie);
+        }
+    });
+});
